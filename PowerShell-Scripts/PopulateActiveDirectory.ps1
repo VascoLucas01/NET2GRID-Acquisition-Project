1e37b9afba29691e04a5f8b33b9c90f50b3cf225
Vasco Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-03-23 20:52:59 +0000

    Add PowerShell-Scripts Directory
    Add PopulateActiveDirectory.ps1

---
+++ PowerShell-Scripts/PopulateActiveDirectory.ps1
@@ -0,0 +1,122 @@
+# Script : PopulateActiveDirectory.ps1
+# Purpose: Manipulates the .csv file in order to add new users to the respective Organizational Unit
+# Why    : It saves time. Instead of insert an user one by one, the script can add a thousand of users at once.
+
+ [CmdletBinding()]
+param (
+    [Parameter(Mandatory=-$True)]
+    [string]$filePath
+)
+
+
+Write-Verbose "Starting script PopulateActiveDirectory.ps1 ..."
+
+# The variable ADUsersInfo stores the content of the file that is imported
+$ADUsersInfo = Import-Csv $filePath -Delimiter ","
+
+
+foreach ($User in $ADUsersInfo){
+    # read information of each user in $ADUsersInfo
+
+    $firstName      = $User.FirstName
+    $middleInitials = $User.MiddleInitials
+    $lastName       = $User.LastName
+    $fullName       = $User.FullName
+    $username       = $User.SamAccountName
+    $password       = $User.Password
+    $displayName    = $User.DisplayName
+    $OU             = $User.OU
+    $office         = $User.Office
+    $email          = $User.Email
+    $webPage        = $User.WebPage
+    $jobTitle       = $User.JobTitle
+    $department     = $User.Department
+    $company        = $User.Company
+    $mainPhone      = $User.MainPhone
+    $homePhone      = $User.HomePhone
+    $mobilePhone    = $User.MobilePhone
+    $fax            = $User.Fax
+    $streetAddress  = $User.Street
+    $city           = $User.City
+    $state          = $User.State
+    $postalCode     = $User.PostalCode
+
+
+"--------------------------------------------------------------------------------"
+    Write-Verbose "First Name          : $firstName "
+    Write-Verbose "Middle Initials     : $middleInitials"
+    Write-Verbose "Last Name           : $lastName"
+    Write-Verbose "Full Name           : $fullName"
+    Write-Verbose "Username            : $username"
+    Write-Verbose "Password            : $password"
+    Write-Verbose "Display Name        : $displayName"
+    Write-Verbose "Organizational Unit : $OU"
+    Write-Verbose "Office              : $office"
+    Write-Verbose "Email               : $email"
+    Write-Verbose "Web Page            : $webPage"
+    Write-Verbose "Job Title           : $jobTitle"
+    Write-Verbose "Department          : $department"
+    Write-Verbose "Company             : $company"
+    Write-Verbose "Main Phone          : $mainPhone"
+    Write-Verbose "Home Phone          : $homePhone"
+    Write-Verbose "Mobile Phone        : $mobilePhone"
+    Write-Verbose "Fax                 : $fax"
+    Write-Verbose "Street Address      : $streetAddress"
+    Write-Verbose "City                : $city"
+    Write-Verbose "State               : $state"
+    Write-Verbose "Postal Code         : $postalCode"
+"--------------------------------------------------------------------------------"
+
+
+   # if the organizational unit exists
+   if( Get-ADOrganizationalUnit -Filter "Name -eq '$OU'" ){
+        Write-Warning "*****************  The Organizational Unit $OU
+exists *****************"
+   }
+   # if the organizational unit not exists
+   # Organizational Units' creation
+   else{
+        New-ADOrganizationalUnit -Name $OU -Path “DC=net2grid,DC=globexpower,DC=com” -Description “NET2GRID”
+        Write-Host "The Organizational Unit $OU was created." -ForegroundColor blue
+   }
+
+
+    # if the user exists
+    if( Get-ADUser -Filter { SamAccountName -eq $username } ){
+        Write-Warning "*****************  The user $username exists *****************"
+    }
+    # if the user not exists
+    # user's creation
+    else {
+        New-ADUser `
+            -GivenName $firstName `
+            -Initials $middleInitials `
+            -Surname $lastName `
+            -Name $fullName `
+            -SamAccountName $username `
+            -AccountPassword (ConvertTo-secureString $password -AsPlainText -Force) -ChangePasswordAtLogon $True `
+            -DisplayName $displayName `
+            -Office $office `
+            -EmailAddress $email `
+            -HomePage $webPage `
+            -Title $jobTitle `
+            -Department $department `
+            -Company $company `
+            -OfficePhone $mainPhone `
+            -HomePhone $homePhone `
+            -MobilePhone $mobilePhone `
+            -Fax $fax `
+            -StreetAddress $streetAddress `
+            -City $city `
+            -State $state `
+            -PostalCode $postalCode `
+            -path "OU=$ou, DC=NET2GRID, DC=globexpower, DC=com"
+
+
+        Write-Host "The user account $username was created." -ForegroundColor blue
+    }
+
+    Write-Verbose "Ending script PopulateActiveDirectory.ps1 ..."
+
+
+}
