6fb78903c6d3b4715e0ae68cfd22c5e0f567e644
Vasco Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-03-26 16:09:11 +0100

    Update ADForestConfig.ps1

--- PowerShell-Scripts/ADForestConfig.ps1
+++ PowerShell-Scripts/ADForestConfig.ps1
@@ -10,18 +10,18 @@ $Password = Read-Host -Prompt   'Enter SafeMode Admin Password' -AsSecureString
 
 # Creates Forest and makes server Domain Controller
 $Net2grid = @{
-CreateDnsDelegation = $false
-DatabasePath = 'C:\Windows\NTDS'
-DomainMode = 'WinThreshold'
-DomainName = 'net2grid.globexpower.com'
-DomainNetbiosName = 'NET2GRID'
-ForestMode = 'WinThreshold'
-InstallDns = $true
-LogPath = 'C:\Windows\NTDS'
-NoRebootOnCompletion = $true
+CreateDnsDelegation           = $false
+DatabasePath                  = 'C:\Windows\NTDS'
+DomainMode                    = 'WinThreshold'
+DomainName                    = 'net2grid.globexpower.com'
+DomainNetbiosName             = 'NET2GRID'
+ForestMode                    = 'WinThreshold'
+InstallDns                    = $true
+LogPath                       = 'C:\Windows\NTDS'
+NoRebootOnCompletion          = $true
 SafeModeAdministratorPassword = $Password
-SysvolPath = 'C:\Windows\SYSVOL'
-Force = $true
+SysvolPath                    = 'C:\Windows\SYSVOL'
+Force                         = $true
 }
 
 Install-ADDSForest @Net2grid
