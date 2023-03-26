f858fd2a48851da2891ea40cf54ef24302c0d93e
Vasco Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-03-26 15:15:17 +0100

    Update WindowsServer2019Config.ps1

--- PowerShell-Scripts/WindowsServer2019Config.ps1
+++ PowerShell-Scripts/WindowsServer2019Config.ps1
@@ -1,12 +1,12 @@
-  # Script : WindowsServer2019Config.ps1
-  # Purpose: Assigns a static IPv4 address to the machine and renames it based on user input. Checks if DNS Server, Management Tools, NPAS and AD-DS are installed, if they are not, installs them including all ManagementTools and Subfeatures.
-  # Why    : It saves time.
+# Script : WindowsServer2019Config.ps1
+# Purpose: Assigns a static IPv4 address to the machine and renames it based on user input. Checks if DNS Server, Management Tools, NPAS and AD-DS are installed, if they are not, installs them including all ManagementTools and Subfeatures.
+# Why    : It saves time.
   
 # Initialize the variables that contains the IP address, prefix, length, adapter, and default gateway to assign
-$ip = '192.168.2.10'
-$prefix = '24'
-$gateway = '192.168.2.1'
-Write-Output "What interface do you want to release the  IP addresses?"
+$ip           = '192.168.2.10'
+$prefix       = '24'
+$gateway      = '192.168.2.1'
+Write-Output  "What interface do you want to release the  IP addresses?"
 
 # Shows the user all the available adapters
 Get-NetAdapter
@@ -46,40 +46,43 @@ switch ($interface) {
  # Checks if DNS Server, NPAS, ManagementTools, and AD-Domain-Services are installed
  $dnsServer = Get-WindowsFeature DNS
  $mgmtTools = Get-WindowsFeature RSAT-AD-Tools
- $adds = Get-WindowsFeature AD-Domain-Services
- $npas = Get-WindowsFeature NPAS
+ $adds      = Get-WindowsFeature AD-Domain-Services
+ $npas      = Get-WindowsFeature NPAS
  
  # If DNS Server is not installed, install it
  if ($dnsServer.Installed -ne $true) {
-  Write-Host "DNS Server is not installed. Installing..."
-  Install-WindowsFeature -Name DNS -IncludeAllSubFeature -IncludeManagementTools
-  Write-Host "DNS Server is now installed"
+    Write-Host "DNS Server is not installed. Installing..."
+    Install-WindowsFeature -Name DNS -IncludeAllSubFeature -IncludeManagementTools
+    Write-Host "DNS Server is now installed"
  } else {
-  Write-Warning "*********** DNS Server is already installed ***********"
+    Write-Warning "*********** DNS Server is already installed ***********"
  }
+ 
  # If ManagementTools are not installed, install it
  if ($mgmtTools.Installed -ne $true) {
-  Write-Host "RSAT-AD-Tools (ManagementTools) is not installed. Installing..."
-  Install-WindowsFeature -Name RSAT-AD-Tools -IncludeAllSubFeature -IncludeManagementTools
-  Write-Host "RSAT-AD-Tools (ManagementTools) is now installed"
+    Write-Host "RSAT-AD-Tools (ManagementTools) is not installed. Installing..."
+    Install-WindowsFeature -Name RSAT-AD-Tools -IncludeAllSubFeature -IncludeManagementTools
+    Write-Host "RSAT-AD-Tools (ManagementTools) is now installed"
  } else {
-  Write-Warning "*********** RSAT-AD-Tools (ManagementTools) is already installed ***********"
+    Write-Warning "*********** RSAT-AD-Tools (ManagementTools) is already installed ***********"
  }
+ 
  # If ADDS is not installed, install it
  if ($adds.Installed -ne $true) {
-  Write-Host "AD-Domain-Services is not installed. Installing..."
-  Install-WindowsFeature -Name AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
-  Write-Host "AD-Domain-Services is now installed"
+    Write-Host "AD-Domain-Services is not installed. Installing..."
+    Install-WindowsFeature -Name AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
+    Write-Host "AD-Domain-Services is now installed"
  } else {
-  Write-Warning "*********** AD-Domain-Services is already installed ***********"
+    Write-Warning "*********** AD-Domain-Services is already installed ***********"
  }
+ 
   # If NPAS is not installed, install it
  if ($npas.Installed -ne $true) {
-  Write-Host "NPAS is not installed. Installing..."
-  Install-WindowsFeature -Name NPAS -IncludeAllSubFeature -IncludeManagementTools
-  Write-Host "NPAS is now installed"
+    Write-Host "NPAS is not installed. Installing..."
+    Install-WindowsFeature -Name NPAS -IncludeAllSubFeature -IncludeManagementTools
+    Write-Host "NPAS is now installed"
  } else {
-  Write-Warning "*********** NPAS is already installed ***********"
+    Write-Warning "*********** NPAS is already installed ***********"
  }
  
  Restart-Computer
