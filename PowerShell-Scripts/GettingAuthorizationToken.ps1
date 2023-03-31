6db2aa5de965728806cac0cc9faae05c0cff9550
Vasco Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-04-01 00:00:47 +0100

    Update GettingAuthorizationToken.ps1

--- PowerShell-Scripts/GettingAuthorizationToken.ps1
+++ PowerShell-Scripts/GettingAuthorizationToken.ps1
@@ -3,7 +3,7 @@ Import-Module UMN-Google
 # Set security protocol to TLS 1.2 to avoid TLS errors
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Google API Authozation
+# Google API Authorization
 $scope = "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file"
 $certPath = "<YOUR_PATH_TO_YOUR_CERTIFICATE_FILE>"
 $iss = "<YOUR_SERVICE_ACCOUNTs_EMAIL_ADDRESS>"
