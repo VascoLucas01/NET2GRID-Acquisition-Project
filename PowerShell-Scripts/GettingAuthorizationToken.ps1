6fe1b07675376b6f78129208eb7428760465534d
Vasco Lucas <110473841+VascoLucas01@users.noreply.github.com>
2023-03-31 23:58:50 +0100

    Create GettingAuthorizationToken.ps1

---
+++ PowerShell-Scripts/GettingAuthorizationToken.ps1
@@ -0,0 +1,19 @@
+Import-Module UMN-Google
+
+# Set security protocol to TLS 1.2 to avoid TLS errors
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Google API Authozation
+$scope = "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file"
+$certPath = "<YOUR_PATH_TO_YOUR_CERTIFICATE_FILE>"
+$iss = "<YOUR_SERVICE_ACCOUNTs_EMAIL_ADDRESS>"
+$certPswd = "<YOUR_PRIVATE_KEY_PASSWORD>"
+try {
+    $accessToken = Get-GOAuthTokenService -scope $scope -certPath $certPath -certPswd $certPswd -iss $iss
+} catch {
+    $err = $_.Exception
+    $err | Select-Object -Property *
+    "Response: "
+    $err.Response
+}
+
